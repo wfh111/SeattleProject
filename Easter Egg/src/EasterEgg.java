8bbb774cb7da34eb4e62654f4f4cbe5e1e4c33e2
giovannagraciani <giovannagraciani@d-10-16-29-61.dhcp4.washington.edu>
2017-10-19 16:13:12 -0700

    Added javadoc for my part - everyone else please add your own!

--- Easter Egg/src/EasterEgg.java
+++ Easter Egg/src/EasterEgg.java
@@ -1,27 +1,66 @@
 import java.util.*;
+/*
+ * This is Iteration 0 of an Easter Egg. When triggered the Easter Egg
+ * will present the user with information about the team
+ * 
+ * User Stories:
+ * As a user I want to see something interesting when I trigger an Easter Egg
+ * As the product manager I want an Easter Egg that shows the team
+ * As a developer I want to my name in the "We are . . . " Easter Egg
+ * 
+ * @authors Team Seattle
+ * @version 0
+ * @since 2017-10-19
+ */
 public class EasterEgg {
-
+	/*
+	 * This method intereacts with the user and then calls each
+	 * team member's method to display information to the user
+	 * @param args 
+	 * @return void
+	 */
 	public static void main(String[] args) {
+		/*
+		 * User interface
+		 */
 		Scanner s = new Scanner(System.in);
 		String c;
 		System.out.println("Press 'e' to activate easter egg");
 		c = s.next();
+		
+		/*
+		 *  Do not print "We are..." message until after user indicates to do so 
+		 */
 		while(!c.equals("e")) {
 			System.out.println("Easter egg not activated.");
 			System.out.println("Press 'e' to activate easter egg");
 			c = s.next();
 		}
+		System.out.println("We are... TEAM SEATTLE!");
 		walterMethod();
 		brianMethod();
+		//Print information about team member Giovanna
+		giovannaMethod():
 
 	}
+	
 	public static void walterMethod() {
 		System.out.println("My name is Walter Hanson");
 		System.out.println("I like the Seahawks and Mariners");
 	}
+	
 	public static void brianMethod() {
 		System.out.println("System Online.");
 		System.out.println("Welcome back Brian Khang.");
 		System.out.println("Loading favorite game preferences and uncaging the panda and delivering sushi.");
+	}
+	
+	/*
+	 * This method prints out Giovanna's name and a fun fact about her
+	 * @return void - unused
+	 */
+	public static void giovannaMethod() {
+		System.out.println("I am Giovanna. I like watching Game of Thrones.");
+		
 	}
 }
