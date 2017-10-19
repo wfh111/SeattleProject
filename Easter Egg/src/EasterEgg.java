52cee16d6df2f7aa92431e6fbb8ad6e0b68ffcd8
wfh111 <wfh661@gmail.com>
2017-10-19 13:03:36 -0700

    Added my method and created main() for easter egg

--- Easter Egg/src/EasterEgg.java
+++ Easter Egg/src/EasterEgg.java
@@ -1,11 +1,24 @@
-
+import java.util.*;
 public class EasterEgg {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-//tests
-//Dab on walter
-		// SZECHUAN SAUCE MEME LORD DAB ON BRAIN!! v2!! - Dino
+		Scanner s = new Scanner(System.in);
+		String c;
+		System.out.println("Press 'e' to activate easter egg");
+		c = s.next();
+		while(!c.equals("e")) {
+			System.out.println("Easter egg not activated.");
+			System.out.println("Press 'e' to activate easter egg");
+			c = s.next();
+		}
+		walterMethod();
+//		System.out.println("Hello World");
+////tests
+////Dab on walter
+//		// SZECHUAN SAUCE MEME LORD DAB ON BRAIN!! v2!! - Dino
 	}
-
+	public static void walterMethod() {
+		System.out.println("My name is Walter Hanson");
+		System.out.println("I like the Seahawks and Mariners");
+	}
 }
