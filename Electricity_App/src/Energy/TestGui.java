7bb5389aeb168d941c8a7404123eaef16c1b8ec1
Ekizochikku <bri565@uw.edu>
2017-11-30 18:16:13 -0800

    Put in testGui

---
+++ Electricity_App/src/Energy/TestGui.java
@@ -0,0 +1,83 @@
+package Energy;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JMenuBar;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TestGui {
+
+	private JFrame frame;
+	private boolean aTrigger = true;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestGui window = new TestGui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TestGui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		
+
+		JLabel lblCloudStatusOn = new JLabel("Cloud Status: ON");
+		frame.getContentPane().add(lblCloudStatusOn);
+		
+		JButton btnNewButton = new JButton("History");
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Estimator");
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.getContentPane().add(menuBar);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Cloud Sync");
+		rdbtnNewRadioButton.setSelected(true);
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(aTrigger == true) {
+					lblCloudStatusOn.setText("Cloud Status: OFF");
+					aTrigger = false;
+				}
+				else {
+					lblCloudStatusOn.setText("Cloud Status: ON");
+					aTrigger = true;
+				}
+			}
+		});
+		menuBar.add(rdbtnNewRadioButton);
+	}
+
+}
