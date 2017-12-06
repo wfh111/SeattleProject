3d973cb0bcbdc084ebc5673d994ff64aa3b05a4c
Dino <dhadzic@uw.edu>
2017-12-05 18:35:33 -0800

    Material class done

--- Electricity_App/src/Energy/Mat.java
+++ Electricity_App/src/Energy/Mat.java
@@ -1,1 +1,32 @@
-//Mat
+package Energy;
+
+
+public class Mat {
+	
+	private double KwH;
+	
+	private double price;
+	
+	private String type;
+	
+	
+	public Mat(String type, double KwH, double price) {
+		this.KwH = KwH;
+		this.type = type;
+		this.price = price;
+	}
+	
+	public double getKwH() {
+		return KwH;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public String getMaterialType() {
+		return type;
+	}
+	
+	
+}
