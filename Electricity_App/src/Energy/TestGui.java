51a5e5bc65a9ae7c62bc23528c454afc75b6407c
giovannagraciani <giovannagraciani@d-10-16-2-231.dhcp4.washington.edu>
2017-12-05 17:47:44 -0800

    Updated for ComparingFrame

--- Electricity_App/src/Energy/TestGui.java
+++ Electricity_App/src/Energy/TestGui.java
@@ -14,7 +14,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class TestGui extends JFrame{
-
+	//comment
 	private JFrame frame;
 	private boolean aTrigger = true;
 
